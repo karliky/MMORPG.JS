5ab91e794bbfed248955132ad7b0494fe91a2ac1
Carlos Hernández Gómez <karliky@gmail.com>
2013-06-16 19:46:30 +0200

    Implemented first version of the tile editor

--- mmorpg_gui.js
+++ mmorpg_gui.js
@@ -12,13 +12,35 @@ MMORPG.prototype.GUI = {
 
         this.getTemplate("assets/templates/tile_selector.html",function(html){
             this.insertTemplate(html);
-            $( "#tile_selector" ).draggable({ handle: ".title" });
+            
             var create_tile_set = document.getElementById("create_tile_set");
+            var create_tile_from_url = document.getElementById("create_tile_set");
+            var tile_mapper = document.getElementById("tile_mapper");
             create_tile_set.addEventListener("click",function(){
-                console.log("Creando ");
+                if(tile_mapper.style.display != "block"){
+                    tile_mapper.style.display = "block";
+                    tile_mapper.style.left = ((window.innerWidth / 2) - (tile_mapper.clientWidth / 2))+"px";
+                }
             }.bind(this), false);
+            create_tile_from_url.addEventListener("click",function(){
+                var url = prompt("Image URL","assets/TileA2.png");
+                this.makeGrid(url);
+            }.bind(this), false);
+            $( "#tile_selector" ).draggable({ handle: ".title" });
+            $( "#tile_mapper" ).draggable({ handle: ".title:first-child" });
 
         }.bind(this));
+    },
+    makeGrid : function(path){
+        var img = document.createElement("image");
+        img.onload = function(){
+            var grid_mapper = document.getElementById("grid_mapper");
+            grid_mapper.appendChild(img);
+        };
+        img.onerror = function(){
+            alert("There was an error trying to load the image.");
+        };
+        img.src = path;
     },
     insertTemplate : function(html){
         var el = document.createElement('div');
